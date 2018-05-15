a774ab8766f6d1d12248756dcd16907d0be6936c
n4n0 <n4n0@tuta.io>
2018-05-15 11:09:21 +0200

    Update chan-scraper.py

--- chan-scraper.py
+++ chan-scraper.py
@@ -9,8 +9,12 @@
 # TODO: make script compatible with Python 2.7 and Python 3.x
 # TODO: make downloader handle unicode characters (i.e. cyrillic)
 # TODO: have only a hrefs in array if the a has an img child (line 60)
+# TODO: args handling for uninterrupted downloading
+# TODO: custom download directory
+# TODO: check for text-links on URL and paste them into txt-file
 
 # import some libraries
+from __future__ import print_function
 from bs4 import BeautifulSoup
 try:
     import urllib.request as urllib2
@@ -32,7 +36,7 @@ print("""
 | (__| | | | (_| | | | | \__ \ (__| | | (_| | |_) |  __/ |
  \___|_| |_|\__,_|_| |_| |___/\___|_|  \__,_| .__/ \___|_|
                                             | |
-  Download picrelated from any chan         |_|       v0.1
+  Download picrelated from any chan         |_|       v0.2
 
 """)
 
@@ -42,7 +46,7 @@ ua = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'
       'Chrome/23.0.1271.64 Safari/537.11'}
 
 # get URL from user
-if sys.version_info[:2] <= (2,7):
+if sys.version_info[:2] <= (2, 7):
     user_input = raw_input
 else:
     user_input = input
@@ -70,9 +74,10 @@ for img in soup.select('a[href$=jpg],'
     if img_url not in scrape:
         scrape.append(img_url)
 
-# set up path names for downloads
+# set up path names and other variables for downloads
 home = os.path.expanduser("~")
 fpath = os.path.join(home, "chanscraper", base, path, thread)
+skipped = 0
 
 # create directory if necessary
 if not os.path.exists(fpath):
@@ -84,11 +89,15 @@ for img in scrape:
     full_path = os.path.join(fpath, file_name)
     if not os.path.exists(full_path):
         filedata = urllib2.urlopen(img)
+        if skipped > 0:
+            print("\n")
         print("Grabbing " + img + "...")
         with open(full_path, 'wb') as f:
             f.write(filedata.read())
         print("OK!")
     else:
-        print("File already exists, skipping...")
+        skipped += 1
+        print("File already exists, skipping [" +
+              str(skipped) + "]... \r", end="")
 
-print("\nDownloaded " + str(len(scrape)) + " files!")
+print("\nDownloaded " + str(len(scrape) - skipped) + " new files!")
